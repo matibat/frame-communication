2815cf9bf0313f74f326aa70628c8e7737e7b719
Mati <matibat@gmail.com>
2019-08-09 17:41:40 -0700

    Clean webpack setup. Initialized npm module

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'eval-source-map'
+};
