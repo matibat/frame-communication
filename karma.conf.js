bd71f10cce5d46b0d0ca299c391498347f100e41
Mati <matibat@gmail.com>
2019-08-09 20:06:50 -0700

    Setup testing frameworks with karma, mocha and chai

---
+++ karma.conf.js
@@ -0,0 +1,26 @@
+module.exports = function(config) {
+    config.set({
+        frameworks: ['mocha', 'chai'],
+    
+        files: [
+            './tests/*.js'
+        ],
+
+        singleRun: true,
+
+        browsers: ['Chrome'],
+    
+        client: {
+            mocha: {
+                // change Karma's debug.html to the mocha web reporter
+                reporter: 'html',
+        
+                // require specific files after Mocha is initialized
+                // require: [require.resolve('bdd-lazy-var/bdd_lazy_var_global')],
+        
+                // // custom ui, defined in required file above
+                // ui: 'bdd-lazy-var/global',
+            }
+        }
+    });
+};
