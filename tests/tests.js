8f2789fddf65e3f013b5e74ece1d5be466703839
Mati <matibat@gmail.com>
2019-08-21 20:53:59 -0700

    Setup html for tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,19 +1,18 @@
 'use strict'
 
-import { findAllFrames } from '../src/core';
+import { getAllFrames } from '../src/core';
 
 const assert = chai.assert;
 
 describe('core.findAllFrames()', function() {
     let frames;
 
-    this.beforeEach(function() {
+    this.beforeAll(function() {
         console.log(JSON.stringify(FrameCommunication));
-        frames = findAllFrames();
+        frames = getAllFrames();
     });
 
     it('returns an array', function(done, reject) {
-        frames = findAllFrames();
         assert.isArray(frames)
         done();
     });
@@ -25,4 +24,12 @@ describe('core.findAllFrames()', function() {
         });
         done();
     });
+});
+
+describe('query frames by app name', function() {
+    it('returns empty array if no one was found', function() {
+        return FrameCommunication.findFrames('wrongAppName');
+    })
+
+    it('finds some direct children frame');
 });
